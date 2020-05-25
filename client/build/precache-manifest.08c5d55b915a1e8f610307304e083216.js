7793952768c175d5612c4b654ecc0de373963072
nicholasykng <nicholasykng@gmail.com>
2020-05-25 15:03:24 -0700

    finalize changes

---
+++ client/build/precache-manifest.08c5d55b915a1e8f610307304e083216.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9cb926139fe43318e51b5c0fa4fa4c2",
+    "url": "/nationalpark-frontend/index.html"
+  },
+  {
+    "revision": "190f0854dcc0925633a5",
+    "url": "/nationalpark-frontend/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "304931553bcc7211a5ed",
+    "url": "/nationalpark-frontend/static/css/main.e32eae89.chunk.css"
+  },
+  {
+    "revision": "190f0854dcc0925633a5",
+    "url": "/nationalpark-frontend/static/js/2.bffafad7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nationalpark-frontend/static/js/2.bffafad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304931553bcc7211a5ed",
+    "url": "/nationalpark-frontend/static/js/main.fd49860e.chunk.js"
+  },
+  {
+    "revision": "a776de95a3373533ea70",
+    "url": "/nationalpark-frontend/static/js/runtime-main.92c46200.js"
+  }
+]);
